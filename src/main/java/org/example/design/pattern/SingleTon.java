1952a9f4f56ac2556b5a45b67f0f27f2d3d38c2b
wangjianfei <wangjianfei@cmss.chinamobile.com>
2021-06-02 16:57:52 +0800

    Summary:ddd
    Code Source From:Self Code
    Description:
    Jira: #DBMT-
    市场项目编号（名称）:[Optional]

--- src/main/java/org/example/design/pattern/SingleTon.java
+++ src/main/java/org/example/design/pattern/SingleTon.java
@@ -1,4 +1,24 @@
 package org.example.design.pattern;
 
+/**
+ * 单例模式
+ */
 public class SingleTon {
+
+    private volatile static SingleTon SINGLE_TON = null;
+
+    private SingleTon() {
+    }
+
+    public static SingleTon getInstance() {
+        //刚进来判断不需要直接锁
+        if (SINGLE_TON == null) {
+            synchronized (SingleTon.class){
+                if (SINGLE_TON == null) {
+                    SINGLE_TON = new SingleTon();
+                }
+            }
+        }
+        return SINGLE_TON;
+    }
 }
